aab3809aec6429aaec80d3861424387975f54e67
MartinFiori <martinfiori@outlook.com>
2023-12-26 18:09:08 -0300

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 const sequelize = require("./src/config/db.js");
 
-const PORT = process.env.PORT || 8080;
-const path = require("path");
+// const PORT = process.env.PORT || 8080;
+// const path = require("path");
 // Importo las asociaciones
-require("./src/models/associations.js");
-const { saveDataBase } = require("./src/controllers/products_controller.js");
+// require("./src/models/associations.js");
+// const { saveDataBase } = require("./src/controllers/products_controller.js");
 
 //midlewares
 app.use(express.json());
@@ -28,20 +28,22 @@ app.get("/prueba", (req, res) => {
 });
 //ROUTES
 
-app.use("/backend", require("./src/routes/users_routes.js"));
-app.use("/backend", require("./src/routes/products_routes.js"));
-app.use("/backend", require("./src/routes/category_routes.js"));
-app.use("/backend", require("./src/routes/payments_routes.js"));
-app.use("/images", express.static(path.join(__dirname, "/assets/images")));
+// app.use("/backend", require("./src/routes/users_routes.js"));
+// app.use("/backend", require("./src/routes/products_routes.js"));
+// app.use("/backend", require("./src/routes/category_routes.js"));
+// app.use("/backend", require("./src/routes/payments_routes.js"));
+// app.use("/images", express.static(path.join(__dirname, "/assets/images")));
 
-sequelize
-  .authenticate()
-  .then(() => console.log("DB-CONNECTED"))
-  .catch((err) => console.log(err.message));
+// sequelize
+//   .authenticate()
+//   .then(() => console.log("DB-CONNECTED"))
+//   .catch((err) => console.log(err.message));
 
-(async function () {
-  await sequelize.sync({ force: false });
-  console.log("save database");
-  saveDataBase();
-  app.listen(PORT, () => console.log("Listening on port ", PORT));
-})();
+// (async function () {
+//   await sequelize.sync({ force: false });
+//   console.log("save database");
+//   saveDataBase();
+//   app.listen(PORT, () => console.log("Listening on port ", PORT));
+// })();
+
+app.listen(PORT, () => console.log("Listening on port ", PORT));
