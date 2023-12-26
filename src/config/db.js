4c3d256b7a3a46e621d9f696b04a958b4d93ea7d
MartinFiori <martinfiori@outlook.com>
2023-12-26 18:27:29 -0300

    cambiando el db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,13 +4,29 @@ const encodedPassword = encodeURIComponent(process.env.DB_PASSWORD);
 
 const { DB_USER, DB_HOST, DB_DATABASE } = process.env;
 
+// const sequelize = new Sequelize(
+//   `postgres://eltwcqez_postgres:${encodedPassword}@${DB_HOST}:5432/${DB_DATABASE}`,
+//   {
+//     host: DB_HOST,
+//     dialect: "postgres",
+//     port: 5432,
+//     logging: false,
+//   }
+// );
+
 const sequelize = new Sequelize(
   `postgres://eltwcqez_postgres:${encodedPassword}@${DB_HOST}:5432/${DB_DATABASE}`,
+  // `${DATABASE_URL}`,
   {
-    host: DB_HOST,
-    dialect: "postgres",
-    port: 5432,
     logging: false,
+    native: false,
+    // configuración
+    // dialectOptions: {
+    // 	ssl: {
+    // 		require: true,
+    // 		rejectUnauthorized: false,
+    // 	},
+    // },
   }
 );
 
